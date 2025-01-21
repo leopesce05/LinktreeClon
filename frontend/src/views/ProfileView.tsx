b6779842e7ca9959794306a281c8b4713aee8620
Leonardo Pesce <leonardo.pesce2003@gmail.com>
2025-01-21 18:33:05 -0300

    Query Client para acceso a cache de queries

--- frontend/src/views/ProfileView.tsx
+++ frontend/src/views/ProfileView.tsx
@@ -1,10 +1,28 @@
+import {useForm} from 'react-hook-form';
+import ErrorMessage from '../components/ErrorMessage';
+import {useQueryClient} from '@tanstack/react-query';
+import { ProfileForm, User } from '../types';
+
+
 
 export default function ProfileView() {
 
+    const queryClient = useQueryClient();
+    const data : User = queryClient.getQueryData(['user'])!
+
+    const {register, handleSubmit, formState : {errors}} = useForm<ProfileForm>({defaultValues : {
+        handle: data.handle, 
+        description: data.description
+    }});
+    
+    const handleUserProfileForm=(formData : ProfileForm)=>{
+        console.log(formData);
+    }
+    
     return (
         <form 
             className="bg-white p-10 rounded-lg space-y-5"
-            onSubmit={() => {}}
+            onSubmit={handleSubmit(handleUserProfileForm)}
         >
             <legend className="text-2xl text-slate-800 text-center">Editar Información</legend>
             <div className="grid grid-cols-1 gap-2">
@@ -15,7 +33,11 @@ export default function ProfileView() {
                     type="text"
                     className="border-none bg-slate-100 rounded-lg p-2"
                     placeholder="handle o Nombre de Usuario"
+                    {...register("handle", {
+                        required: "El handle es obligatorio",
+                    })}
                 />
+                {errors.handle && <ErrorMessage>{errors.handle.message}</ErrorMessage> }
             </div>
 
             <div className="grid grid-cols-1 gap-2">
@@ -25,6 +47,7 @@ export default function ProfileView() {
                 <textarea
                     className="border-none bg-slate-100 rounded-lg p-2"
                     placeholder="Tu Descripción"
+                    {...register("description")}
                 />
             </div>
 
